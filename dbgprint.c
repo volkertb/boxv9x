c78154bd644c3d6e56c192dce316177204bed567
Michal Necasek <mnecasek@yahoo.com>
2022-06-20 20:20:05 +0200

    Corrected copyright years.

--- dbgprint.c
+++ dbgprint.c
@@ -1,6 +1,6 @@
 /*****************************************************************************
 
-Copyright (c) 2012-2022  Michal Necasek
+Copyright (c) 2022  Michal Necasek
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
